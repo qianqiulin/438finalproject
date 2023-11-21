5e59633efc6477c45348f98b57dabdb06810beb8
LeoOfECUST <75165274+LeoOfECUST@users.noreply.github.com>
2023-11-20 18:37:08 -0600

    Connect with Firestore database

--- WUBET-LOGIN/LoginViewController.swift
+++ WUBET-LOGIN/LoginViewController.swift
@@ -32,6 +32,7 @@ class LoginViewController: UIViewController {
                 }
             }
         }
+        
     }
     
     /*
--- WUBET-LOGIN/ViewController.swift
+++ WUBET-LOGIN/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import FirebaseFirestore
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
         // Do any additional setup after loading the view.
+        getCollection()
     }
+    var db: Firestore!
+    func getCollection(){
+        db.collection("matchinfo").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                guard let querySnapshot = querySnapshot else {
+                    print("No documents found.")
+                    return
+                }
 
+                for document in querySnapshot.documents {
+                    print("\(document.documentID): \(document.data())")
+                }
+            }
+        }
+    }
+    
+    
+    
 
 }
 
